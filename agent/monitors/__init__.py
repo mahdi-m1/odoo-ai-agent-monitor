d9a406ddd0cfa62abbdf9ad17aff7c891bc82925
mahdi-m1 <72039446+mahdi-m1@users.noreply.github.com>
2026-09-11 13:50:43 +0300

    Add monitors package init

---
+++ agent/monitors/__init__.py
@@ -0,0 +1,5 @@
+from .appointments_monitor import AppointmentsMonitor
+from .news_monitor import NewsMonitor
+from .legislation_monitor import LegislationMonitor
+
+__all__ = ["AppointmentsMonitor", "NewsMonitor", "LegislationMonitor"]
